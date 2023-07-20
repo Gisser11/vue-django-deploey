8703c495849b92e4667bc6c2ce113bc51ed75467
GISSER77 <artem-sergeev-02@mail.ru>
2023-07-20 13:17:10 +0300

    123

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <div class="p-8 bg-white shadow-lg rounded-lg">
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-primary-light to-primary-dark">
+    <div class="p-8 bg-gray-200 shadow-lg rounded-lg">
       <h1 class="text-4xl font-bold text-gray-800 mb-4">Привет!</h1>
       <router-link to="/CRUD"><p class="text-lg text-gray-600 mb-4">Для перехода на основную страницу нажмите на меня!</p></router-link>
     </div>
@@ -14,4 +14,7 @@ export default {
 </script>
 
 <style>
+.bg-gradient-to-br {
+  background-image: linear-gradient(to bottom right, #C6F6D5, #9AE6E6);
+}
 </style>
